eef924fa4bd58611b270968d75b3e8960d64d005
StaneCobalt <scsladek91@gmail.com>
2018-09-15 15:41:57 -0500

    Add repeating list of profiles

--- cs-profile/src/app/profile-list/profile-list.component.ts
+++ cs-profile/src/app/profile-list/profile-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-profile-list',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-list.component.css']
 })
 export class ProfileListComponent implements OnInit {
+  profiles: Object[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    this.profiles = [
+      {
+        img_path: "../../assets/images/temp.png", name:"Stephen Sladek",
+        location:"Gordonville, MO",college_status:"Senior",
+        languages:"C, C++, C#, Java, JavaScript",interests:"Virtual Reality",
+        organizations:"ACM-SEMO, CS Club, SIGAI"
+      },
+      {
+        img_path: "../../assets/images/temp.png", name:"",
+        location:"",college_status:"",
+        languages:"",interests:"",
+        organizations:""
+      },
+      {
+        img_path: "../../assets/images/temp.png", name:"",
+        location:"",college_status:"",
+        languages:"",interests:"",
+        organizations:""
+      },
+    ];
   }
+
+  ngOnInit() {}
 
 }
