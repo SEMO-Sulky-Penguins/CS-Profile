06cb5256f3f8d55080fb286369af3312096feccc
StaneCobalt <scsladek91@gmail.com>
2018-10-06 20:15:48 -0500

    Update profiles and styles
    Entire cards are clickable now and light up red when hovered over. Some properties of the profile class have been converted to arrays. New profile view now loads the array items in blocks instead of as a single string.

--- cs-profile/src/app/mock-profiles.ts
+++ cs-profile/src/app/mock-profiles.ts
@@ -1,59 +1,59 @@
 import {Profile} from './profile';
  
-export const PROFILES: Profile[] = [{
-        id: 0,
-        img_path: "../../assets/images/anon.jpg", name:"Stephen Sladek",
-        location:"Gordonville, MO",college_status:"Senior",
-        languages:"C, C++, C#, Java, JavaScript",interests:"Virtual Reality",
-        organizations:"ACM-SEMO, CS Club, SIGAI"
-      },
-      {
-        id: 2,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-      {
-        id: 3,
-        img_path: "../../assets/images/Derek_Mandl.jpg", name:"Derek Mandl",
-        location:"Manchester, MO",college_status:"Senior",
-        languages:"C, C++, Java, Python",interests:"Compilers, Static Languages",
-        organizations:"ACM-SEMO, Camera Arts Association"
-      },
-      {
-        id: 4,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-      {
-        id: 5,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-      {
-        id: 6,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-      {
-        id: 7,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-      {
-        id: 8,
-        img_path: "../../assets/images/anon.jpg", name:"Anonymous",
-        location:"---",college_status:"---",
-        languages:"",interests:"",
-        organizations:""
-      },
-    ];
+export const PROFILES: Profile[] = [
+  {
+    id: 0,
+    img_path: "../../assets/images/anon.jpg", name:"Stephen Sladek",
+    major:"Information Systems",
+    location:"Gordonville, MO",college_status:"Senior",
+    languages:["C","C++", "C#", "Java", "JavaScript"],
+    interests:["Virtual Reality", "Biometrics"],
+    organizations:["ACM-SEMO", "CS Club", "SIGAI"]
+  },
+  {
+    id: 2,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"Cybersecurity",
+    location:"---",college_status:"Freshman",
+    languages:[],interests:[],organizations:[]
+  },
+  {
+    id: 3,
+    img_path: "../../assets/images/Derek_Mandl.jpg", name:"Derek Mandl",
+    major:"Computer Science",
+    location:"Manchester, MO",college_status:"Senior",
+    languages:["C", "C++", "Java", "Python"],
+    interests:["Compilers", "Static Languages"],
+    organizations:["ACM-SEMO", "Camera Arts Association"]
+  },
+  {
+    id: 4,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"---",location:"---",college_status:"---",
+    languages:[],interests:[],organizations:[]
+  },
+  {
+    id: 5,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"---",location:"---",college_status:"---",
+    languages:[],interests:[],organizations:[]
+  },
+  {
+    id: 6,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"---",location:"---",college_status:"---",
+    languages:[],interests:[],organizations:[]
+  },
+  {
+    id: 7,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"---",location:"---",college_status:"---",
+    languages:[],interests:[],organizations:[]
+  },
+  {
+    id: 8,
+    img_path: "../../assets/images/anon.jpg", name:"Anonymous",
+    major:"---",location:"---",college_status:"---",
+    languages:[],interests:[],organizations:[]
+  },
+];
--- cs-profile/src/app/profile.ts
+++ cs-profile/src/app/profile.ts
@@ -2,9 +2,10 @@ export class Profile {
   id: number;
   img_path: string;
   name: string;
+  major: string;
   location: string;
   college_status: string;
-  languages: string;
-  interests: string;
-  organizations: string;
+  languages: Array<string>;
+  interests: Array<string>;
+  organizations: Array<string>;
 }
